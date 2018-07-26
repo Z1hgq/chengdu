7c6d73e088865e48e36de5dfb2279567cb4cd1f6
Z1hgq <1134348143@qq.com>
2018-07-26 08:29:28 +0800

    身份证姓名电话号码校验

--- js/rec.js
+++ js/rec.js
@@ -33,56 +33,77 @@ jQuery(document).ready(function() {
     console.log($('#pic_path').html());
     pic_path = $('#pic_path').html();
     if (red_area != '' && arr[0] != '' && pic_path != '' && re_name != '' && re_mobile != '' && re_id != '' && red_name != '' && red_mobile != '' && red_id != '' && th_type != '' && th_title != '') {
-      $.ajax({
-        type: "POST",
-        url: "../backWeb/php/info_up.php",
-        data: {
-          re_name: re_name,
-          re_mobile: re_mobile,
-          re_id: re_id,
-          red_name: red_name,
-          red_mobile: red_mobile,
-          red_id: red_id,
-          th_type: th_type,
-          th_title: th_title,
-          th_content:arr[0],
-          red_area: red_area,
-          pub_time: pub_time,
-          pic_path: pic_path
-        },
-        dataType: 'json',
-        success: function(msg) {
-
-        },
-        complete: function(data) {
-          //console.log(data.responseText);
-          var jobj = JSON.parse(data.responseText);
-          console.log(jobj);
-          if (jobj.status == 0) {
-            $('#re_name').val('');
-            $('#re_mobile').val('');
-            $('#re_id').val('');
-            $('#red_name').val('');
-            $('#red_mobile').val('');
-            $('#red_id').val('');
-            $('#th_type').val('');
-            $('#th_title').val('');
-            UE.getEditor('editor').setContent('');
-            $.message('上传成功');
-          } else if (jobj.status == 1) {
+      if(!ischina(re_name) || !ischina(red_name)){
+        $.message({
+          message: '请输入2-4位的中文名字',
+          type: 'warning'
+        });
+      }else{
+        if(!isTelCode(re_mobile) || !isTelCode(red_mobile)){
+          $.message({
+            message: '请输入正确的电话号码',
+            type: 'warning'
+          });
+        }else{
+          if(!isIdentify(re_id) || !isIdentify(red_id)){
             $.message({
-              message: '上传失败',
-              type: 'error'
+              message: '请输入正确的身份证号',
+              type: 'warning'
+            });
+          }else{
+            $.ajax({
+              type: "POST",
+              url: "../backWeb/php/info_up.php",
+              data: {
+                re_name: re_name,
+                re_mobile: re_mobile,
+                re_id: re_id,
+                red_name: red_name,
+                red_mobile: red_mobile,
+                red_id: red_id,
+                th_type: th_type,
+                th_title: th_title,
+                th_content:arr[0],
+                red_area: red_area,
+                pub_time: pub_time,
+                pic_path: pic_path
+              },
+              dataType: 'json',
+              success: function(msg) {
+
+              },
+              complete: function(data) {
+                //console.log(data.responseText);
+                var jobj = JSON.parse(data.responseText);
+                console.log(jobj);
+                if (jobj.status == 0) {
+                  $('#re_name').val('');
+                  $('#re_mobile').val('');
+                  $('#re_id').val('');
+                  $('#red_name').val('');
+                  $('#red_mobile').val('');
+                  $('#red_id').val('');
+                  $('#th_type').val('');
+                  $('#th_title').val('');
+                  UE.getEditor('editor').setContent('');
+                  $.message('上传成功');
+                } else if (jobj.status == 1) {
+                  $.message({
+                    message: '上传失败',
+                    type: 'error'
+                  });
+                }
+              },
+              fail: function(msg) {
+                $.message({
+                  message: '上传失败',
+                  type: 'error'
+                });
+              }
             });
           }
-        },
-        fail: function(msg) {
-          $.message({
-            message: '上传失败',
-            type: 'error'
-          });
         }
-      });
+      }
     } else {
       $.message({
         message: '请输入完整',
@@ -124,4 +145,19 @@ function handleStateChange() {
       $.message('上传图片成功');
     }
   }
+}
+/*校验是否中文名称组成 */
+function ischina(str) {
+    var reg=/^[\u4E00-\u9FA5]{2,4}$/;   /*定义验证表达式*/
+    return reg.test(str);     /*进行验证*/
+}
+/*校验电话码格式 */
+function isTelCode(str) {
+    var reg= /^((0\d{2,3}-\d{7,8})|(1[3584]\d{9}))$/;
+    return reg.test(str);
+}
+// 检验身份证号
+function isIdentify(str) {
+    var reg= /^(\d{6})(\d{4})(\d{2})(\d{2})(\d{3})([0-9]|X)$/;
+    return reg.test(str);
 }
